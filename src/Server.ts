183eedb67b331a1a1ac44c6e28fc9bfc25498001
nehaagoel <neha.goel@successive.tech>
2020-01-30 16:52:55 +0530

    feat: Add http handlers

--- src/Server.ts
+++ src/Server.ts
@@ -1,22 +1,32 @@
 import * as express from 'express';
 import IConfig from './config/IConfig';
+import * as bodyParser from 'body-parser';
+import { errorHandler, notFoundRoute } from './libs';
 
 class Server {
     app: express.Application;
     constructor(private config: IConfig) {
         this.app = express();
     }
-    bootstrap() {
+    bootstrap(): Server {
+        this.bodyParser();
         this.setupRoutes();
         return this;
     }
-
-    setupRoutes() {
+  bodyParser(): void {
+        const { app } = this;
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+        this.app.use(bodyParser.json());
+    }
+    setupRoutes(): void {
+        const { app } = this;
         this.app.get('/health-check', (req: express.Request, res: express.Response) => {
             res.send('I am OK');
+            this.app.use(notFoundRoute);
+            this.app.use(errorHandler);
         });
         }
-        run() {
+        run(): Server {
             this.app.listen(this.config.port, ( err) => {
                 if (err) {
                     console.log(err);
