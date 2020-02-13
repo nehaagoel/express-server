fb1eb63baf4db2f0d192e89fc403b46267580654
nehaagoel <neha.goel@successive.tech>
2020-02-13 16:48:35 +0530

    feat: setup of all entities

--- src/repositories/user/UserModel.ts
+++ src/repositories/user/UserModel.ts
@@ -2,17 +2,8 @@ import * as mongoose from 'mongoose';
 import UserSchema from './UserSchema';
 import IUserModel from './IUserModel';
 
-const toConvert = {
-    transfers: (doc: any, ret: any) => {
-        ret.id = ret._id;
-        delete ret._id;
-        delete ret._v;
-    }
-};
 export const userSchema = new UserSchema ({
     collection: 'Users',
-    toJson: toConvert,
-    toObject: toConvert,
-});
+  });
 
 export const userModel: mongoose.Model<IUserModel> = mongoose.model<IUserModel>('user', userSchema, 'Users', true);
